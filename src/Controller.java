c21adcd3eafb3c8c8f79fec3ce8e9ce0126c3d1d
Tallgren <toby9847@gmail.com>
2022-12-15 15:10:50 +0100

    Started developing FileHandler, (full participation)

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,11 @@
+import java.io.IOException;
+
 public class Controller {
+    private String wordFile = "src/Words.txt";
+    public Controller() throws IOException {
+        FileHandler fh = new FileHandler();
+
+        fh.writeHighscore("Robin",999);
+        System.out.println(fh.generateWord(wordFile));
+    }
 }
